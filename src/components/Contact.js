bccdd602a4bde884114bd329a1631673936361e2
Raihanul Haque <raihanulhaque@rocketmail.com>
2020-09-03 14:23:34 +0600

    onClick event on Contact.js comp now toggles contact info

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,21 +2,25 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Contact extends Component {
-    state = {};
+    state = {
+        showContactInfo: false
+    };
 
     onShowClick = (e) => {
-        console.log(e.target);
+        // console.log(e.target);
+        this.setState({ showContactInfo: !this.state.showContactInfo })
     }
 
     render() {
         const { name, email, phone } = this.props.contact;
+        const { showContactInfo } = this.state
         return (
             <div className="card card-body mb-3">
                 <h4>{name} <i onClick={this.onShowClick} className="fas fa-sort-down"></i></h4>
-                <ul className="list-group">
+                {showContactInfo ? (<ul className="list-group">
                     <li className="list-group-item">Email: {email}</li>
                     <li className="list-group-item">Phone: {phone}</li>
-                </ul>
+                </ul>) : null}
             </div>
         )
     }
--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -28,14 +28,14 @@ state = {
         const { contacts } = this.state;
 
         return (
-            <div>
+            <React.Fragment>
                 {contacts.map(contact => (
                     <Contact 
                         key={contact.id}
                         contact={contact} 
                     />
                 ))}
-            </div>
+            </React.Fragment>
         )
     }
 }
