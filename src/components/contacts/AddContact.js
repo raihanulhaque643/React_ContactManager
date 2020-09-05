9ea9a8ca4607667ac54d6ba0cb771d38c19b1f3b
Raihanul Haque <raihanulhaque@rocketmail.com>
2020-09-05 15:41:03 +0600

    form validation added

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -7,7 +7,8 @@ class AddContact extends Component {
     state = {
         name: '',
         email: '',
-        phone: ''
+        phone: '',
+        errors: {}
     };
 
     onChange = e => this.setState({ [e.target.name]: e.target.value })
@@ -16,6 +17,22 @@ class AddContact extends Component {
         e.preventDefault();
         
         const { name, email, phone } = this.state;
+
+        // Check for errors 
+        if (name === '') {
+            this.setState({errors: {name: 'Name is required'}});
+            return;
+        }
+
+        if (email === '') {
+            this.setState({errors: {email: 'Email is required'}});
+            return;
+        }
+
+        if (phone === '') {
+            this.setState({errors: {phone: 'Phone is required'}});
+            return;
+        }
 
         const newContact = {
             id: uuid(),
@@ -30,13 +47,14 @@ class AddContact extends Component {
         this.setState({
             name: '',
             email: '',
-            phone: ''
+            phone: '',
+            errors: {}
         });
 
     };
 
     render() {
-        const { name, email, phone } = this.state;
+        const { name, email, phone, errors } = this.state;
 
         return (
             <Consumer>
@@ -54,6 +72,7 @@ class AddContact extends Component {
                                 placeholder="Enter Name"
                                 value={name}
                                 onChange={this.onChange}
+                                error={errors.name}
                             />
 
                             <TextInputGroup 
@@ -63,6 +82,7 @@ class AddContact extends Component {
                                 placeholder="Enter Email"
                                 value={email}
                                 onChange={this.onChange}
+                                error={errors.email}
                             />
 
                             <TextInputGroup 
@@ -71,6 +91,7 @@ class AddContact extends Component {
                                 placeholder="Enter Phone"
                                 value={phone}
                                 onChange={this.onChange}
+                                error={errors.phone}
                             />
                             
                             <input type="submit" value="Add Contact" className="btn btn-block btn-light"/>
