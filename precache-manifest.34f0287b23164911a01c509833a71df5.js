47995bc72c714095ffcee9f46a3eac40f76c3ad4
Raihanul Haque <raihanulhaque@rocketmail.com>
2020-09-06 18:27:33 +0600

    Updates

---
+++ precache-manifest.34f0287b23164911a01c509833a71df5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08e24a2447d01326b49ada03ec3a5b6f",
+    "url": "/React_ContactManager/index.html"
+  },
+  {
+    "revision": "af47b33b38df3d06e6f5",
+    "url": "/React_ContactManager/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "a7566f8733fdb0e9a0a5",
+    "url": "/React_ContactManager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "af47b33b38df3d06e6f5",
+    "url": "/React_ContactManager/static/js/2.a4479be0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React_ContactManager/static/js/2.a4479be0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7566f8733fdb0e9a0a5",
+    "url": "/React_ContactManager/static/js/main.23c5b09d.chunk.js"
+  },
+  {
+    "revision": "42b38608ed38323b4e1c",
+    "url": "/React_ContactManager/static/js/runtime-main.df69518c.js"
+  }
+]);
